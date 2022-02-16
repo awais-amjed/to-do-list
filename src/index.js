98f6b98fd8672f6a0a828bec8b250c81d6ab1af2
Awais Amjed <awaisamjed171@gmail.com>
2022-02-16 18:37:12 +0500

    Change ToDo List file name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './assets/images/more.png';
 import './assets/images/delete.png';
 import './assets/images/checked.png';
 import './assets/images/clipboard.png';
-import ToDoList from './modules/ToDoList.js';
+import ToDoList from './modules/to-do-list.js';
 import { populateAll } from './modules/html_functions.js';
 
 const toDoList = new ToDoList();
