e86ad09c16ab7f42445c069f3a54b7c7d4c5c440
Awais Amjed <awaisamjed171@gmail.com>
2022-02-23 19:33:00 +0500

    Add test to check if a task already exists

--- src/unit_tests/to-do-list.test.js
+++ src/unit_tests/to-do-list.test.js
@@ -6,20 +6,30 @@ import ToDoList from '../modules/to-do-list.js';
 
 describe('Manipulate Tasks List', () => {
   const todo = new ToDoList();
+  document.body.innerHTML = `
+   <ul id="tasks-list">
+
+   </ul>
+   `;
+
+  const tasksList = document.getElementById('tasks-list');
 
   test('Add a task', () => {
-    document.body.innerHTML = `
-       <ul id="tasks-list">
-
-       </ul>
-      `;
-
-    const tasksList = document.getElementById('tasks-list');
     const previousLength = todo.tasks.length;
     todo.addNewTask('Test 1');
 
     expect(todo.tasks).toHaveLength(previousLength + 1);
     expect(tasksList.childElementCount).toBe((previousLength + 1) * 2);
+
+    // Check if all the nodes are created in DOM
+    expect(tasksList.querySelector('.task')).toBeInstanceOf(Node);
+    expect(tasksList.querySelector('.task .task-detail')).toBeInstanceOf(Node);
+    expect(tasksList.querySelector('button')).toBeInstanceOf(Node);
+  });
+
+  test('Test if a Task Exists', () => {
+    expect(todo.taskExists('Test 1')).toBe(true);
+    expect(todo.taskExists('Test 2')).toBe(false);
   });
 
   test('Remove task from the list', () => {
